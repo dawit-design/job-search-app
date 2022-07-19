a3b197180bc3349b997395553169a63e71e3f3b8
dawit-endaylalu <dawitawegchow@gmail.com>
2022-07-19 16:33:46 -0400

    created here screen

---
+++ src/components/Hero.vue
@@ -0,0 +1,12 @@
+<template>
+  <main>
+    <section class="flex flex-col h-screen pt-10 pb-20 bg-yellow-200">I can be your hero baby!</section>
+  </main>
+</template>
+
+<script>
+  export default {
+    name: "Hero",
+  }
+</script>
+
