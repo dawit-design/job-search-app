a7751ba924985b019e688b0cb0da786859768066
dawit-endaylalu <dawitawegchow@gmail.com>
2022-07-19 19:53:21 -0400

    headline for hero screen created

---
+++ src/components/Headline.vue
@@ -0,0 +1,20 @@
+<template>
+  <section>
+    <h1>Build For Everyone</h1>
+    <h2>Find your next job at sira ferlagi!</h2>
+  </section>
+</template>
+
+<script>
+  export default {
+    name: "Headline",
+    data () {
+      return {
+        sample: "Hello",
+      }
+    },
+    created() {
+      console.log("Hey, I've been created. My data exists!", this.sample)
+    }
+  }
+</script>
