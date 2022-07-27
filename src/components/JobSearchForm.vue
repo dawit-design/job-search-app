b094926fabe73a9e5269bc34dd8107cb40fd806c
dawit-endaylalu <dawitawegchow@gmail.com>
2022-07-27 09:04:24 -0400

    styled job search from

--- src/components/JobSearchForm.vue
+++ src/components/JobSearchForm.vue
@@ -6,17 +6,41 @@
     <div class="flex flex-nowrap flex-1 h-full text-base font-light">
       <div class="relative flex items-center flex-1 h-full pr-3">
         <label class="absolute left-0 -top-10">Role</label>
-        <input type="text" placeholder="Software Engineer" class="w-full text-lg font-normal focus:outline-none"/>
+        <input
+          type="text"
+          placeholder="Software Engineer"
+          class="w-full text-lg font-normal focus:outline-none"
+        />
       </div>
-      <span></span>
-      <div></div>
+      <span
+        class="
+        flex items-center 
+        h-full px-3 
+        border-l border-r border-brand-gray-3 
+        bg-brand-gray-2"
+        >IN</span
+      >
+      <div class="relative flex items-center flex-1 h-full pl-3">
+        <label class="absolute left-0 -top-10">Where?</label>
+        <input
+          type="text"
+          placeholder="New York City"
+          class="w-full text-lg font-normal focus:outline-none"
+        />
+      </div>
     </div>
+    <action-button text="Search" type="secondary" class="rounded-r-3xl"/>
   </form>
+
 </template>
 
 <script>
+import ActionButton from "@/components/ActionButton.vue"
 export default {
   name: "JobSearchForm",
+  components: {
+    ActionButton,
+  }
 };
 </script>
 
