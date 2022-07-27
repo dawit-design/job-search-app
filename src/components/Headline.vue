39a12098b243c6f847726d481eb240122713dc15
dawit-endaylalu <dawitawegchow@gmail.com>
2022-07-27 08:54:47 -0400

    styled job search from

--- src/components/Headline.vue
+++ src/components/Headline.vue
@@ -1,46 +1,50 @@
 <template>
   <section class="mb-16">
-    <h1 class="font-bold tracking-tighter text-8xl mb-14" data-test="action-phrase">
-      <span :class="actionsClasses">{{action}}</span>
-      <br/>
-      for everyone</h1>
+    <h1
+      class="font-bold tracking-tighter text-8xl mb-14"
+      data-test="action-phrase"
+    >
+      <span :class="actionsClasses">{{ action }}</span>
+      <br />
+      for everyone
+    </h1>
     <h2 class="text-3xl font-light">Find your next job at sira ferlagi!</h2>
   </section>
 </template>
 
 <script>
-import nextElementInList from '@/utils/nextElementInList'
+import nextElementInList from "@/utils/nextElementInList";
 
-  export default {
-    name: "Headline",
-    data () {
+export default {
+  name: "Headline",
+  data() {
+    return {
+      action: "Build",
+      interval: null,
+    };
+  },
+  created() {
+    this.changeTitle();
+  },
+  beforeUnmount() {
+    clearInterval(this.interval);
+  },
+  computed: {
+    actionsClasses() {
       return {
-        action: "Build",
-        interval: null,
-      }
+        [this.action.toLowerCase()]: true,
+      };
     },
-    created() {
-      this.changeTitle()
-    },
-    beforeUnmount () {
-      clearInterval(this.interval)
-    },
-    computed: {
-      actionsClasses(){
-        return {
-          [this.action.toLowerCase()]: true,
-        }
-      }
-    },
-    methods: {
-      changeTitle() {
-        this.interval = setInterval(() => {
+  },
+  methods: {
+    changeTitle() {
+      this.interval = setInterval(() => {
         const actions = ["Build", "Create", "Design", "Code"];
         this.action = nextElementInList(actions, this.action);
-        }, 3000)
-      }
-    }
-  }
+      }, 3000);
+    },
+  },
+};
 </script>
 
 <style scoped>
@@ -51,7 +55,7 @@ import nextElementInList from '@/utils/nextElementInList'
   color: #34a853;
 }
 .design {
-  color:#f9ab00
+  color: #f9ab00;
 }
 .code {
   color: #d93025;
--- src/components/JobSearchForm.vue
+++ src/components/JobSearchForm.vue
@@ -1,15 +1,16 @@
 <template>
   <form
-    class="
-    flex 
-    items-center 
-    w-full h-12 
-    mt-14 
-    border border-solid border-brand-gray-3 
-    rounded-3xl
-    "
+    class="flex items-center w-full h-12 mt-14 border border-solid border-brand-gray-3 rounded-3xl"
   >
-  <font-awesome-icon :icon="['fas', 'search']" class="ml-4 mr-3"/>
+    <font-awesome-icon :icon="['fas', 'search']" class="ml-4 mr-3" />
+    <div class="flex flex-nowrap flex-1 h-full text-base font-light">
+      <div class="relative flex items-center flex-1 h-full pr-3">
+        <label class="absolute left-0 -top-10">Role</label>
+        <input type="text" placeholder="Software Engineer" class="w-full text-lg font-normal focus:outline-none"/>
+      </div>
+      <span></span>
+      <div></div>
+    </div>
   </form>
 </template>
 
