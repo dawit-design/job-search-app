ae439114faa1ad2025418e54fe3a1d8f19e56a69
dawit-endaylalu <dawitawegchow@gmail.com>
2022-07-26 20:07:19 -0400

    created a job search form and cleared bolier plate from the hero components

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,34 +1,27 @@
 <template>
   <main>
-    <section class="flex flex-col h-screen pt-10 pb-20 bg-yellow-200">
-     <div class="grid grid-cols-12">
-        <div class="col-start-1 col-span-1 border border-blue-700">
-          1 column
-        </div>
-        <div class="col-start-2 col-span-5 border border-blue-700">
+    <section class="flex flex-col h-screen pt-10 pb-20">
+      <div class="grid grid-cols-12">
+        <div class="col-start-1 col-span-1"></div>
+        <div class="col-start-2 col-span-5">
           <headline />
-        </div >
-        <div class="col-start-7 col-span-5 border border-blue-700">
-          5 columns
+          <job-search-form />
         </div>
-        <div class="col-start-12 col-span-1 border border-blue-700">
-          column 1
-        </div>
-     </div>
-     </section>
+        <div class="col-start-7 col-span-5"></div>
+        <div class="col-start-12 col-span-1"></div>
+      </div>
+    </section>
   </main>
 </template>
 
 <script>
-
-import Headline from "@/components/Headline.vue"
-  export default {
-    name: "Hero",
-    components: {
-      Headline,
-    }
-  }
+import Headline from "@/components/Headline.vue";
+import JobSearchForm from "@/components/JobSearchForm.vue";
+export default {
+  name: "Hero",
+  components: {
+    Headline,
+    JobSearchForm,
+  },
+};
 </script>
-
-
-
---
+++ src/components/JobSearchForm.vue
@@ -0,0 +1,11 @@
+<template>
+  <form >I will be in a better form</form>
+</template>
+
+<script>
+  export default {
+    name: 'JobSearchForm',
+  }
+</script>
+
+<style></style>
