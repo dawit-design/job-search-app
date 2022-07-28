9848b13ae59be9aa147117a964702dd02b7efd1b
dawit-endaylalu <dawitawegchow@gmail.com>
2022-07-27 20:35:13 -0400

    created reusable text input component

--- src/components/JobSearch/JobSearchForm.vue
+++ src/components/JobSearch/JobSearchForm.vue
@@ -6,7 +6,7 @@
     <div class="flex flex-nowrap flex-1 h-full text-base font-light">
       <div class="relative flex items-center flex-1 h-full pr-3">
         <label class="absolute left-0 -top-10">Role</label>
-        <text-input placeholder="Software Engineer"/>
+        <text-input placeholder="Software Engineer" @handleInput="updateRole"/>
       </div>
       <span
         class="flex items-center h-full px-3 border-l border-r border-brand-gray-3 bg-brand-gray-2"
@@ -35,6 +35,11 @@ export default {
       role: "",
       location: "",
     };
+  },
+  methods: {
+    updateRole(payload) {
+      console.log(payload);
+    },
   },
 };
 </script>
