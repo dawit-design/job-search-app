ac55861a406bb729f7b17f0ead4e05487a534935
dawit-endaylalu <dawitawegchow@gmail.com>
2022-08-19 15:03:55 -0400

    fixed test suit issues for router-link

--- tests/unit/components/Navigation/MainNav.test.js
+++ tests/unit/components/Navigation/MainNav.test.js
@@ -1,15 +1,29 @@
-import { shallowMount } from "@vue/test-utils";
+import { shallowMount, RouterLinkStub } from "@vue/test-utils";
 import MainNav from "@/components/Navigation/MainNav.vue";
 import { data } from "autoprefixer";
 
 describe("MainNav", () => {
   it("displays company name", () => {
-    const wrapper = shallowMount(MainNav);
+    const wrapper = shallowMount(MainNav, {
+      global: {
+        stubs: {
+          "router-link": RouterLinkStub,
+        },
+      },
+    });
     expect(wrapper.text()).toMatch("Sira Felagi");
   });
   it("displays menu item for navigation", () => {
-    const wrapper = shallowMount(MainNav);
-    const navigationMenuItems = wrapper.findAll("[data-test='main-nav-list-item']");
+    const wrapper = shallowMount(MainNav, {
+      global: {
+        stubs: {
+          "router-link": RouterLinkStub,
+        },
+      },
+    });
+    const navigationMenuItems = wrapper.findAll(
+      "[data-test='main-nav-list-item']"
+    );
     const navigationMenuTexts = navigationMenuItems.map((item) => item.text());
     expect(navigationMenuTexts).toEqual([
       "Teams",
@@ -20,35 +34,53 @@ describe("MainNav", () => {
       "Jobs",
     ]);
   });
-  describe("when the user logged out", () =>{
-    it("prompts user to sign in", () =>{
-      const wrapper = shallowMount(MainNav)
-      const loginButton = wrapper.find("[data-test='login-button']")
-      expect(loginButton.exists()).toBe(true)
-    })
-  })
+  describe("when the user logged out", () => {
+    it("prompts user to sign in", () => {
+      const wrapper = shallowMount(MainNav, {
+        global: {
+          stubs: {
+            "router-link": RouterLinkStub,
+          },
+        },
+      });
+      const loginButton = wrapper.find("[data-test='login-button']");
+      expect(loginButton.exists()).toBe(true);
+    });
+  });
   describe("when the user logs in", () => {
     it("displays user profile picture", async () => {
-      const wrapper = shallowMount(MainNav)
-      let ProfileImage = wrapper.find("[data-test='profile-image']")
-      expect(ProfileImage.exists()).toBe(false)
+      const wrapper = shallowMount(MainNav, {
+        global: {
+          stubs: {
+            "router-link": RouterLinkStub,
+          },
+        },
+      });
+      let ProfileImage = wrapper.find("[data-test='profile-image']");
+      expect(ProfileImage.exists()).toBe(false);
 
-      const loginButton = wrapper.find("[data-test='login-button']")
-      await loginButton.trigger("click")
+      const loginButton = wrapper.find("[data-test='login-button']");
+      await loginButton.trigger("click");
 
-      ProfileImage = wrapper.find("[data-test='profile-image']")
-      expect(ProfileImage.exists()).toBe(true)
-    })
+      ProfileImage = wrapper.find("[data-test='profile-image']");
+      expect(ProfileImage.exists()).toBe(true);
+    });
     it("displays subnavigation menu with additional information", async () => {
-      const wrapper = shallowMount(MainNav);
+      const wrapper = shallowMount(MainNav, {
+        global: {
+          stubs: {
+            "router-link": RouterLinkStub,
+          },
+        },
+      });
       let subnav = wrapper.find("[data-test='subnav']");
-      expect(subnav.exists()).toBe(false)
+      expect(subnav.exists()).toBe(false);
 
-      const loginButton = wrapper.find("[data-test='login-button']")
-      await loginButton.trigger("click")
+      const loginButton = wrapper.find("[data-test='login-button']");
+      await loginButton.trigger("click");
 
       subnav = wrapper.find("[data-test='subnav']");
-      expect(subnav.exists()).toBe(true)
-    })
-  })
+      expect(subnav.exists()).toBe(true);
+    });
+  });
 });
