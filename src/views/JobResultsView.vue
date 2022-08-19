e33f7308657a05014f676ab766cc1618ecbdfc1c
dawit-endaylalu <dawitawegchow@gmail.com>
2022-08-19 13:54:44 -0400

    navigating with router.push

--- src/views/JobResultsView.vue
+++ src/views/JobResultsView.vue
@@ -1,9 +1,17 @@
 <template>
-    <h1 class="text-6xl font-bold">Job Results View</h1>
+<div>
+  <h1 class="text-6xl font-bold">Job Results View</h1>
+  <button @click="goToHome">Go to HomePage</button>
+</div>
 </template>
 
 <script>
 export default {
   name: "JobResultsView",
+  methods: {
+    goToHome() {
+      this.$router.push("/")
+    }
+  }
 }
 </script>
